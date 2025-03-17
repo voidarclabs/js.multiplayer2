f3e7d1ffd1811c94ca4fbbfa20c201f8c71331ae
voidarc <110408795+voidarclabs@users.noreply.github.com>
2025-03-17 11:12:33 +0000

    login system

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const socketio = require('socket.io');
+const fs = require('fs');
+const { type } = require('os');
+
+const port = 9000
+
+let users
+
+loadFiles()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.get('/game', (req, res) => {
+    res.sendFile(path.join(__dirname, 'game', 'game.json'));
+});
+
+// use folder public
+app.use(express.static('public'));
+
+// start server
+const server = app.listen(port, () => {
+    console.log(`Server listening on`, port);
+});
+
+// declare websocket server
+const io = socketio(server)
+
+io.on("connection", (socket) => {
+    console.log(socket.id)
+
+    socket.on("request", (type, data, callback) => {
+        if (type == "login") {
+            if (users[data.username]) {
+                if (users[data.username] == data.password) {
+                    console.log("correct password")
+                } else {
+                    console.log("user found, incorrect password")
+                }
+            } else {
+                console.log("no user found")
+            }
+        }
+    })
+})
+
+function loadFiles() {
+    users = require("./users.json")
+}
+
+function saveToFiles() {
+    fs.writeFileSync("./users.json", users)
+
+    loadFiles()
+}
---
+++ public/main.js
@@ -0,0 +1,33 @@
+const socket = io()
+
+function login() {
+    let usernameBox = document.getElementById("usernameBox")
+    let passwordBox = document.getElementById("passwordBox")
+
+    let loginInfo = {
+        "username":usernameBox.value,
+        "password":passwordBox.value
+    }
+
+    if (loginInfo.username == "" || loginInfo.password == "") {
+        console.log("empty")
+        return
+    } else {
+        let callback = request("login", loginInfo)
+
+        console.log(callback)
+    }
+}
+
+async function request(requestType, data) {
+    return new Promise((resolve, reject) => {
+        socket.emit("request", requestType, data, (callback) => {
+            console.log(callback);
+            if (callback) {
+                resolve(callback);
+            } else {
+                reject(new Error("Request failed"));
+            }
+        });
+    });
+}
